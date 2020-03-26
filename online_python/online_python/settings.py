152a852462ab0a617dcd1467908ef82af3a27bde
zhaojf <zhaojf_xt@teamsun.com.cn>
2020-03-26 20:14:09 +0800

    add

--- online_python/online_python/settings.py
+++ online_python/online_python/settings.py
@@ -75,10 +75,20 @@ WSGI_APPLICATION = 'online_python.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'onlinepython',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
